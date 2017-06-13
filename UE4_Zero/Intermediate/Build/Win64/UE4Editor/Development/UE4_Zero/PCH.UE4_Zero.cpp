a7688746276419e1426d4c57ebb1313123aa14d6
Marcus Callender <m.shute@outlook.com>
2017-06-13 19:11:29 +0100

    Basic UE4 C++ project setup

---
+++ UE4_Zero/Intermediate/Build/Win64/UE4Editor/Development/UE4_Zero/PCH.UE4_Zero.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/Marcus/Desktop/GitHub/UE4-Zero/UE4_Zero/Intermediate/Build/Win64/UE4Editor/Development/UE4_Zero/PCH.UE4_Zero.h"
---
+++ UE4_Zero/Source/UE4_Zero/UE4_ZeroGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "UE4_ZeroGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_ZERO_API AUE4_ZeroGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
